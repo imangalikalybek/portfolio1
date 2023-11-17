41bf53b0368ff51417efdd79304ccfee023070c8
imangalikalybek <57932754+imangalikalybek@users.noreply.github.com>
2023-11-17 16:44:31 +0600

    Create index.js
    171123

---
+++ index.js
@@ -0,0 +1,12 @@
+
+/*
+const h2List = document.querySelectorAll("h2") 
+const btnList = document.querySelectorAll(".btn")
+
+for (let i = 0; i < h2List.length; i++) {
+    btnList[i].addEventListener("click", () => {
+        window.scrollTo(0, h2List[i].offsetTop)
+    })
+} */
+
+
